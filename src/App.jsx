58103aad0958195b128716945791ae1b8b344d83
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-21 12:24:28 -0600

    trying to back track to figure out the instructions from Router Config to Nav Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 
 import React from "react";
@@ -11,6 +11,9 @@ const App = () => {
         <Route path="/blue" element={<h1>Blue</h1>} />
         <Route path="/red" element={<h1>Red</h1>} />
       </Routes>
+      <h1></h1>
+      <div id="navbar">{/* navigation here */}</div>
+      <div id="main-section">{/* routes here */}</div>
     </div>
   );
 };
