cb02bc552a527e961331ae35e19719e12a6f1c77
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-21 14:58:28 -0600

    added home route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Blue from "./Components/Blue";
 import Red from "./Components/Red";
-
+import Home from "./Components/Home";
 
 
 
@@ -12,11 +12,13 @@ const App = () => {
     
     <div id="main-section">
       <div id="navbar">
+        <Link to="/home">Home</Link>
         <Link to="/blue">Blue</Link>
         <Link to="/red">Red</Link>
       </div>
       
       <Routes>
+        <Route path="/home" element={<Home/>}/>
         <Route path="/blue" element={<Blue/>} />
         <Route path="/red" element={<Red/>}/>
       </Routes>
