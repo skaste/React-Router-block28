61464a070a46d98bdd554716bc5decaddb49f714
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-21 11:28:53 -0600

    updated the code for tab 1 in css and jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,4 +2,15 @@ import { useState } from 'react'
 
 import './App.css'
 
+import React from 'react'
 
+const App = () => {
+  return (
+    <div id="container">
+    <h1>Hello React Router!</h1>
+    <div id="navbar">{/* navigation here */}</div>
+    <div id="main-section">{/* routes here */}</div>
+  </div>
+  )
+}
+export default App
