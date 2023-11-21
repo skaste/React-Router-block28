ca9b861a052fec67f93cd0d55534b487e3741569
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-21 11:51:53 -0600

    completed Router config moving on to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
-import './App.css'
-
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
-    <div id="container">
-    <h1>Hello React Router!</h1>
-    <div id="navbar">{/* navigation here */}</div>
-    <div id="main-section">{/* routes here */}</div>
-  </div>
-  )
-}
-export default App
+    <div id="main-section">
+      <Routes>
+        <Route path="/blue" element={<h1>Blue</h1>} />
+        <Route path="/red" element={<h1>Red</h1>} />
+      </Routes>
+    </div>
+  );
+};
+export default App;
